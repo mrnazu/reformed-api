46afdb75fb6622de35e4bf6d74ffca06730f9336
mrnazu <samuelamsalu1998@gmail.com>
2024-07-24 18:33:32 -0700

    Added API routes and default error handler to server.js

--- server.js
+++ server.js
@@ -8,9 +8,16 @@ const searchRoutes = require('./routes/searchRoutes');
 
 app.use(express.json());
 
+// API routes
 app.use('/api/creeds', creedRoutes);
 app.use('/api/metadata', metadataRoutes);
 app.use('/api/search', searchRoutes);
+
+// Default error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
