ddb4793623cce691449e71d3dad8d8107b15a393
mrnazu <samuelamsalu1998@gmail.com>
2024-07-24 18:32:44 -0700

    Remove authMiddleware.js and errorMiddleware.js files

--- middleware/authMiddleware.js
+++
@@ -1,5 +0,0 @@
-const authMiddleware = (req, res, next) => {
-  next();
-};
-
-module.exports = authMiddleware;
--- middleware/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-const errorMiddleware = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-};
-
-module.exports = errorMiddleware;
